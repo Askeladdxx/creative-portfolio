56ce332c6694500ededc5f88c2e1cb80162e1526
Joshua Yves Edellon <joshuadellon.jye@gmail.com>
2023-07-10 19:48:28 +0800

    Added components in homepage

--- quasar-project/src/pages/portfolio/script/portfolio.js
+++ quasar-project/src/pages/portfolio/script/portfolio.js
@@ -1,35 +1,50 @@
+import { ref } from "vue";
+
 export default {
-    data() {
-      return {
-        navbtns: ["about me", "socials", "experience"],
-        buttons: [
-          "Graphic Design",
-          "3D Art",
-          "2D Art",
-          "Pixel Art",
-          "Logo and Mockup",
-        ],
-      };
+  setup() {
+    const slide = ref(1);
+    const autoplay = ref(true);
+
+    const getSlideText = () => {
+      switch (slide.value) {
+        case 1:
+          return "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corporis perferendis, earum cupiditate nobis ullam excepturi corrupti nesciunt autem rerum odit esse! Impedit sed consectetur cumque odio est dicta maiores.";
+        case 2:
+          return "Quo repellendus, quas esse saepe veritatis cum reprehenderit magnam eum delectus quae nemo quasi est velit! Quidem porro reprehenderit fugiat necessitatibus iste!";
+        case 3:
+          return "Some different text for the third slide.";
+        default:
+          return "";
+      }
+    };
+
+    return {
+      slide,
+      autoplay,
+
+      getSlideText,
+    };
+  },
+  data() {
+    return {
+      navbtns: ["home", "about me", "socials", "experience"],
+    };
+  },
+  methods: {
+    handleNavButtonClick(button) {
+      console.log("Button clicked:", button);
+      // Add your logic here to navigate to the desired destination based on the selected button
+      if (button === "home") {
+        this.$router.push("/");
+      } else if (button === "about me") {
+        this.$router.push("/about-me");
+      } else if (button === "socials") {
+        // Navigate to the 3D Art page
+        this.$router.push("/social-media");
+      } else if (button === "experience") {
+        // Navigate to the 2D Art page
+        this.$router.push("/experience");
+      }
     },
-    methods: {
-      handleButtonClick(button) {
-        console.log("Button clicked:", button);
-        // Add your logic here to navigate to the desired destination based on the selected button
-        if (button === "Graphic Design") {
-          this.$router.push("/graphic-design");
-        } else if (button === "3D Art") {
-          // Navigate to the 3D Art page
-          this.$router.push("/3d-art");
-        } else if (button === "2D Art") {
-          // Navigate to the 2D Art page
-          this.$router.push("/2d-art");
-        } else if (button === "Pixel Art") {
-          // Navigate to the Pixel Art page
-          this.$router.push("/pixel-art");
-        } else if (button === "Logo and Mockup") {
-          // Navigate to the Logo and Mockup page
-          this.$router.push("/logo-and-mockup");
-        }
-      },
-    },
-  };
+  },
+};
